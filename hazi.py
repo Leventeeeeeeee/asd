3804e2a6f35dcae6b2f801bc6ded98d0c65a7517
Leventeeeeeeee <117735674+Leventeeeeeeee@users.noreply.github.com>
2022-11-08 13:37:09 +0100

    Add files via upload

---
+++ hazi.py
@@ -0,0 +1,32 @@
+"""
+1
+
+
+for x in range (1,51,1):
+ print(x)
+ 
+ 2
+szam1 = int input("Kérem a szamot"))  
+if szam1 %2==0:
+   print("A szam paros")
+else:
+   print("A szam paratlan")
+"""
+"""
+3.
+szam = int(input("Kérem a szamot"))
+szam1 = int(input("Kerem a szamot"))
+if szam > szam1:
+    print("Az elso  nagyobb ")
+else:
+    print("A masodik nagyobb")
+"""
+"""
+4.
+for x in range (1,6,1):
+  szam = int(input("ADj egy szamot"))
+if szam % 2 == 0:
+    print("A szam paros")
+else:
+    print("A szam paratlan")
+    """
